2e2947d2845c39969a1af64d74a9e2b726046fa3
aishtiaq <awshafishtiaque@gmail.com>
2026-02-03 12:22:31 -0800

    update

--- exercises/ex11.js
+++ exercises/ex11.js
@@ -28,26 +28,49 @@ Implement the functions one by one. The example inputs and outputs below will he
 const PI = 3.14159;
 
 const sphereVolume = function (radius) {
-  // Code here!
+  return (4/3) * PI * Math.pow(radius,3); //V = (4/3)πr^3 
 };
 
-console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); //true
+// console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); //true
 
 const coneVolume = function (radius, height) {
-  // And here!
+  // V = (1/3)πr²h
+  return (1/3) * PI * radius * radius * height
 };
 
-console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); //true
+// console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); //true
 
 const prismVolume = function (height, width, depth) {
-  // Probably here too!
+  return height * width * depth; 
 };
 
-console.log(prismVolume(3, 4, 5) === 60); //true
+// console.log(prismVolume(3, 4, 5) === 60); //true
 
 const totalVolume = function (solids) {
-  // Code here? Yup!
+  //getting each shape of a solid
+  // duck = 2 sphere + 1 cone 
+  // add each shape volume to the final volume 
+  // return the whole volume
+
+  let totalVolume = 0; 
+  for (const solid of solids){
+    // console.log('solid ->', solid);
+    if( solid.type ==='sphere'){
+      totalVolume += sphereVolume(solid.radius)
+    } else if ( solid.type === 'cone'){
+      totalVolume += coneVolume(solid.radius, solid.height);
+    } else if ( solid.type === 'prism') {
+      totalVolume += prismVolume(solid.height, solid.width, solid.depth);
+    }
+  }
+  return totalVolume;
 };
+
+/* 
+    Space Complexity:    O(1) = C = 1; 
+    Time Complexity:     O(n) ; n is solids size ; n is the input size
+
+*/
 
 const largeSphere = {
   type: "sphere",
@@ -65,7 +88,11 @@ const cone = {
   height: 5,
 };
 
-const duck = [largeSphere, smallSphere, cone];
+const myObj = [smallSphere , { type: 'prism', height: 5, depth: 4}]
+
+const duck = [largeSphere, smallSphere, cone ];
+
+// console.log(totalVolume(myObj))
 
 console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000); //true
 
