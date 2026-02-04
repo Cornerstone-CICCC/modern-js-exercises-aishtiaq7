232a54579ec9e81595777ea3e2d5d5b6bca71e6a
aishtiaq <awshafishtiaque@gmail.com>
2026-02-04 09:34:57 -0800

    ex12 done

--- exercises/ex12.js
+++ exercises/ex12.js
@@ -20,8 +20,32 @@ This one is a doozy! We might want to start by creating a helper function called
 
 */
 
+
+
 const chooseRecipe = function (bakeryA, bakeryB, recipes) {
-  // Code here!
+
+  const hasIngredient = function (bakery, ingredient){
+    return bakery.includes(ingredient);
+  }
+
+  for (const recipe of recipes){ 
+    const firstIngredient = recipe.ingredients[0];
+    const secondIngredient = recipe.ingredients[1];
+
+    // console.log(firstIngredient, secondIngredient);
+
+
+    // either ingredient can come from either bakery: 
+    const firstInASecondInB = hasIngredient(bakeryA, firstIngredient) && hasIngredient(bakeryB, secondIngredient);
+
+    const firstInBSecondInA = hasIngredient(bakeryB, firstIngredient) && hasIngredient(bakeryA, secondIngredient);
+
+    if ( firstInASecondInB || firstInBSecondInA){
+      return recipe.name; 
+    }
+
+  }
+  
 };
 
 let bakeryA = ["saffron", "eggs", "tomato paste", "coconut", "custard"];
@@ -60,6 +84,6 @@ recipes = [
   },
 ];
 
-console.log(chooseRecipe(bakeryA, bakeryB, recipes)); //Nima's Famous Dijon Raisins
+// console.log(chooseRecipe(bakeryA, bakeryB, recipes)); //Nima's Famous Dijon Raisins
 
 module.exports = chooseRecipe;
