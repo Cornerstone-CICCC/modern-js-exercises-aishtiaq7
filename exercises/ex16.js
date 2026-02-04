8bd68cb7a101f6dcbd6927d57b6529c8eccf3339
aishtiaq <awshafishtiaque@gmail.com>
2026-02-04 12:20:30 -0800

    update

--- exercises/ex16.js
+++ exercises/ex16.js
@@ -16,8 +16,61 @@ For more information on casing styles, read Wikipedia's Special Case Styles for 
 */
 
 const makeCaze = function (input, caze) {
-  // Put your solution here
+  const applyCasing = (input, casing) => {
+    switch (casing) {
+      case "camel":
+        let wordsArray = input.split(" ");
+        for (let i = 1; i < wordsArray.length; i++) {
+          wordsArray[i] =
+            wordsArray[i][0].toUpperCase() + wordsArray[i].slice(1);
+        }
+        return wordsArray.join("");
+      case "pascal":
+        let wordsArrayPascal = input.split(" ");
+        for (let i = 0; i < wordsArrayPascal.length; i++) {
+          wordsArrayPascal[i] =
+            wordsArrayPascal[i][0].toUpperCase() + wordsArrayPascal[i].slice(1);
+        }
+        return wordsArrayPascal.join("");
+      case "snake":
+        return input.split(" ").join("_");
+      case "kebab":
+        return input.split(" ").join("-");
+      case "title":
+        let wordsArrayTitle = input.split(" ");
+        for (let i = 0; i < wordsArrayTitle.length; i++) {
+          wordsArrayTitle[i] =
+            wordsArrayTitle[i][0].toUpperCase() + wordsArrayTitle[i].slice(1);
+        }
+        return wordsArrayTitle.join(" ");
+      case "vowel":
+        let replaced = input.replace(/[aeiou]/g, (char) => char.toUpperCase());
+        return replaced;
+      case "consonant":
+        return input.replace(/[^aeiou]/g, (char) => char.toUpperCase());
+      case "upper":
+        return input.toUpperCase();
+      case "lower":
+        return input.toLowerCase();
+      default:
+        console.log("default");
+    }
+  };
+
+  if (Array.isArray(caze)){
+    for( const casing of caze) {
+      input = applyCasing(input, casing);
+    }
+    return input; 
+  } else {
+    return applyCasing(input, caze);
+  }
 };
+
+/* 
+  Time Complexity: ?? O(R*n*m);
+  Space Complexity: O(n) n is the size of string input. 
+*/
 
 console.log(makeCaze("this is a string", "camel")); // thisIsAString
 console.log(makeCaze("this is a string", "pascal")); // ThisIsAString
@@ -26,6 +79,6 @@ console.log(makeCaze("this is a string", "kebab")); // this-is-a-string
 console.log(makeCaze("this is a string", "title")); // This Is A String
 console.log(makeCaze("this is a string", "vowel")); // thIs Is A strIng
 console.log(makeCaze("this is a string", "consonant")); // THiS iS a STRiNG
-console.log(makeCaze("this is a string", ["upper", "snake"])); // THIS_IS_A_STRING
+console.log(makeCaze("this is a string", ["lower", "camel", "consonant"])); // THIS_IS_A_STRING
 
 module.exports = makeCaze;
